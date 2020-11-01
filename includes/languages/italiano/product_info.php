5f954831f64f368cfe3264a81e4216aa11b4aac3
Enrico Maria Chellini <43685677+bitit-it@users.noreply.github.com>
2020-11-01 19:20:17 +0100

    Update product_info.php

--- includes/languages/italiano/product_info.php
+++ includes/languages/italiano/product_info.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id$
+  $Id$  edit 01/11/2020
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -8,7 +8,7 @@
   Copyright (c) 2018 osCommerce
 
   Released under the GNU General Public License
-   edit 01/11/2020
+  
 */
 
  
