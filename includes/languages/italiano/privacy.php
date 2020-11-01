4e796fcb6c33d9873b9d52b6c442fce21320ad2e
Enrico Maria Chellini <43685677+bitit-it@users.noreply.github.com>
2020-11-01 19:21:00 +0100

    Update privacy.php

--- includes/languages/italiano/privacy.php
+++ includes/languages/italiano/privacy.php
@@ -1,14 +1,13 @@
 <?php
 /*
-  $Id: privacy.php,v 1.8 2007/06/28 03:13:39 sd_ Exp $
+  $Id$
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
-  Copyright (c) 2003 osCommerce
+
+  Copyright (c) 2002 osCommerce
+
   Released under the GNU General Public License
-  Norwegian language files MS2.2
-   edit 01/11/2020
-  
 */
 
 define('NAVBAR_TITLE', 'Informativa sulla Privacy');
