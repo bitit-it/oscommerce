630d47555a4dcc05b24f3661b76ee04113e45ec8
Enrico Maria Chellini <43685677+bitit-it@users.noreply.github.com>
2020-11-02 21:51:24 +0100

    Update tax_classes.php

--- admin/includes/languages/italiano/tax_classes.php
+++ admin/includes/languages/italiano/tax_classes.php
@@ -25,4 +25,4 @@ define('TEXT_INFO_DELETE_INTRO', 'Sicuro di voler cancellare questo tipo di Tass
 define('TEXT_INFO_HEADING_NEW_TAX_CLASS', 'Nuova tipo di Tassa');
 define('TEXT_INFO_HEADING_EDIT_TAX_CLASS', 'Nuova tipo di Tassa');
 define('TEXT_INFO_HEADING_DELETE_TAX_CLASS', 'Cancella tipo di Tassa');
-?>
+
