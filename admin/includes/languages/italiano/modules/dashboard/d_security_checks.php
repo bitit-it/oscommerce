427aa77d20011d579aec64c1609d9b4b4377d765
Enrico Maria Chellini <43685677+bitit-it@users.noreply.github.com>
2020-11-02 22:14:34 +0100

    Update d_security_checks.php

--- admin/includes/languages/italiano/modules/dashboard/d_security_checks.php
+++ admin/includes/languages/italiano/modules/dashboard/d_security_checks.php
@@ -13,4 +13,3 @@
 define('MODULE_ADMIN_DASHBOARD_SECURITY_CHECKS_TITLE', 'Controllo Sicurezza');
 define('MODULE_ADMIN_DASHBOARD_SECURITY_CHECKS_DESCRIPTION', 'Avvia controllo sicurezza');
 define('MODULE_ADMIN_DASHBOARD_SECURITY_CHECKS_SUCCESS', 'Questa installazione &egrave; stata correttamente configurata per osCommerce Online Merchant!');
-?>
