e02d90202e8d914b0a3967c7cd2cdc398d6dbf79
Enrico Maria Chellini <43685677+bitit-it@users.noreply.github.com>
2020-11-02 22:17:54 +0100

    Update product_notification.php

--- admin/includes/languages/italiano/modules/newsletters/product_notification.php
+++ admin/includes/languages/italiano/modules/newsletters/product_notification.php
@@ -1,12 +1,12 @@
 <?php
 /*
   $Id$
- 
+
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
- 
-  Copyright (c) 2002 osCommerce
- 
+
+  Copyright (c) 2020 osCommerce
+
   Released under the GNU General Public License
 */
 
@@ -21,4 +21,3 @@ define('BUTTON_SELECT', '>>>');
 define('BUTTON_UNSELECT', '<<<');
 define('BUTTON_SUBMIT', 'Invia');
 define('BUTTON_CANCEL', 'Cancella');
-?>
