d28d0b64d51601ff05f776a77afda559cf217ea7
Enrico Maria Chellini <43685677+bitit-it@users.noreply.github.com>
2020-11-02 22:50:03 +0100

    Update tax_rates.php

--- admin/includes/languages/italiano/tax_rates.php
+++ admin/includes/languages/italiano/tax_rates.php
@@ -12,7 +12,7 @@
 
 define('HEADING_TITLE', 'Aliquota Tassa');
 
-define('TABLE_HEADING_TAX_RATE_PRIORITY', 'Priorit&agrave;');
+define('TABLE_HEADING_TAX_RATE_PRIORITY', 'Priorità;');
 define('TABLE_HEADING_TAX_CLASS_TITLE', 'Tipo di Tassa');
 define('TABLE_HEADING_COUNTRIES_NAME', 'Nazione');
 define('TABLE_HEADING_ZONE', 'Stato/Provincia');
@@ -26,7 +26,7 @@ define('TEXT_INFO_CLASS_TITLE', 'Titolo tipo di Tassa:');
 define('TEXT_INFO_COUNTRY_NAME', 'Nazione:');
 define('TEXT_INFO_ZONE_NAME', 'Stato/Provincia:');
 define('TEXT_INFO_TAX_RATE', 'Aliquota Tassa (%):');
-define('TEXT_INFO_TAX_RATE_PRIORITY', 'Le aliquote tassa con la stessa priorit&agrave; sono state aggiunte, altre sono state mescolate.<br><br>Priorit&agrave;:');
+define('TEXT_INFO_TAX_RATE_PRIORITY', 'Le aliquote tassa con la stessa priorità; sono state aggiunte, altre sono state mescolate.<br><br>Priorità:');
 define('TEXT_INFO_RATE_DESCRIPTION', 'Descrizione:');
 define('TEXT_INFO_INSERT_INTRO', 'Inserisci il nuovo tipo di Tassa con i relativi dati');
 define('TEXT_INFO_DELETE_INTRO', 'Sicuro di voler cancellare questa aliquota di Tassa?');
