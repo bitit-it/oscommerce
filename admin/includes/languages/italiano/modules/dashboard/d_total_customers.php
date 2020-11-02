ed9482b06639ba0614d4a5284d6e22aeded9ec7b
Enrico Maria Chellini <43685677+bitit-it@users.noreply.github.com>
2020-11-02 22:15:14 +0100

    Update d_total_customers.php

--- admin/includes/languages/italiano/modules/dashboard/d_total_customers.php
+++ admin/includes/languages/italiano/modules/dashboard/d_total_customers.php
@@ -5,7 +5,7 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2010 osCommerce
+  Copyright (c) 2019 osCommerce
 
   Released under the GNU General Public License
 */
@@ -13,4 +13,4 @@
 define('MODULE_ADMIN_DASHBOARD_TOTAL_CUSTOMERS_TITLE', 'Grafico Totale Clienti (Ultimi 30 giorni)');
 define('MODULE_ADMIN_DASHBOARD_TOTAL_CUSTOMERS_DESCRIPTION', 'Mostra il grafico dei clienti degli ultimi 30 giorni');
 define('MODULE_ADMIN_DASHBOARD_TOTAL_CUSTOMERS_CHART_LINK', 'Totale Clienti');
-?>
+
