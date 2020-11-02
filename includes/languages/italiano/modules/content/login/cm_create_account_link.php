13032e420abf9095e65f3095737b091da119863b
Enrico Maria Chellini <43685677+bitit-it@users.noreply.github.com>
2020-11-02 21:01:00 +0100

    Update cm_create_account_link.php

--- includes/languages/italiano/modules/content/login/cm_create_account_link.php
+++ includes/languages/italiano/modules/content/login/cm_create_account_link.php
@@ -16,4 +16,3 @@
   define('MODULE_CONTENT_LOGIN_HEADING_NEW_CUSTOMER', 'Nuovo Cliente ');
   define('MODULE_CONTENT_LOGIN_TEXT_NEW_CUSTOMER', 'Sono un nuovo cliente.');
   define('MODULE_CONTENT_LOGIN_TEXT_NEW_CUSTOMER_INTRODUCTION', 'Creando un nuovo account da ' . STORE_NAME . ' sarai in grado di acquistare velocemente, essere sempre aggiornato sullo stato dei tuoi ordini e tenere traccia degli ordini che hai effettuato.');
-?>
