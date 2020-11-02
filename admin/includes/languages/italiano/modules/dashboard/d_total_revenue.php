f279c18daeceaeb37cd13465e08b8bd1c2711297
Enrico Maria Chellini <43685677+bitit-it@users.noreply.github.com>
2020-11-02 22:16:12 +0100

    Update d_total_revenue.php

--- admin/includes/languages/italiano/modules/dashboard/d_total_revenue.php
+++ admin/includes/languages/italiano/modules/dashboard/d_total_revenue.php
@@ -5,7 +5,7 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2010 osCommerce
+  Copyright (c) 2019 osCommerce
 
   Released under the GNU General Public License
 */
@@ -13,4 +13,4 @@
 define('MODULE_ADMIN_DASHBOARD_TOTAL_REVENUE_TITLE', 'Grafico Totale Vendite (Ultimi 30 giorni)');
 define('MODULE_ADMIN_DASHBOARD_TOTAL_REVENUE_DESCRIPTION', 'Mostra il grafico del totale delle vendite degli ultimi 30 giorni');
 define('MODULE_ADMIN_DASHBOARD_TOTAL_REVENUE_CHART_LINK', 'Totale Vendite');
-?>
+
