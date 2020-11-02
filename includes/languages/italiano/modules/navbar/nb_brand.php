599ef0a5dd08de947250456c52f2bb126d2568ab
Enrico Maria Chellini <43685677+bitit-it@users.noreply.github.com>
2020-11-02 21:19:23 +0100

    Update nb_brand.php

--- includes/languages/italiano/modules/navbar/nb_brand.php
+++ includes/languages/italiano/modules/navbar/nb_brand.php
@@ -2,10 +2,10 @@
 /*
   $Id$
 
-  osCommerce, Open Source E-Commerce Solutions 
+  osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2016 osCommerce
+  Copyright (c) 2020 osCommerce
 
   Released under the GNU General Public License
 */
