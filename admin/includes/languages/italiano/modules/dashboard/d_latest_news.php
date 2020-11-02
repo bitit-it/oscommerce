3fe85b4fa266574b15ed9acde0016bb1409f22b5
Enrico Maria Chellini <43685677+bitit-it@users.noreply.github.com>
2020-11-02 22:42:43 +0100

    Update d_latest_news.php

--- admin/includes/languages/italiano/modules/dashboard/d_latest_news.php
+++ admin/includes/languages/italiano/modules/dashboard/d_latest_news.php
@@ -13,7 +13,7 @@ define('MODULE_ADMIN_DASHBOARD_LATEST_NEWS_TITLE', 'Ultime News');
 define('MODULE_ADMIN_DASHBOARD_LATEST_NEWS_DESCRIPTION', 'Mostra le ultime news su osCommerce');
 
 define('MODULE_ADMIN_DASHBOARD_LATEST_NEWS_DATE', 'Data');
-define('MODULE_ADMIN_DASHBOARD_LATEST_NEWS_FEED_ERROR', 'Non puoi connetterti ai feed di osCommerce News. Il prossimo tentativo avverr&agrave; tra 24 ore.');
+define('MODULE_ADMIN_DASHBOARD_LATEST_NEWS_FEED_ERROR', 'Non puoi connetterti ai feed di osCommerce News. Il prossimo tentativo avverrà; tra 24 ore.');
 define('MODULE_ADMIN_DASHBOARD_LATEST_NEWS_ICON_NEWS', 'Leggi le ultime notizie osCommerce');
 define('MODULE_ADMIN_DASHBOARD_LATEST_NEWS_ICON_NEWSLETTER', 'Iscriviti alla Newsletter di osCommerce ');
 define('MODULE_ADMIN_DASHBOARD_LATEST_NEWS_ICON_FACEBOOK', 'Diventa un fan di osCommerce su Facebook');
