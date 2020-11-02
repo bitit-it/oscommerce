7fa56cbfb286524b5ee7e0ce0ce37c8a59ced439
Enrico Maria Chellini <43685677+bitit-it@users.noreply.github.com>
2020-11-02 22:17:24 +0100

    Update newsletter.php

--- admin/includes/languages/italiano/modules/newsletters/newsletter.php
+++ admin/includes/languages/italiano/modules/newsletters/newsletter.php
@@ -1,14 +1,13 @@
 <?php
 /*
   $Id$
- 
+
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
- 
-  Copyright (c) 2002 osCommerce
- 
+
+  Copyright (c) 2020 osCommerce
+
   Released under the GNU General Public License
 */
 
 define('TEXT_COUNT_CUSTOMERS', 'Clienti che ricevono Newsletter: %s');
-?>
