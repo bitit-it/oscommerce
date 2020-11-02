00dc893cc3232f9bd6e64f23fe56cd57247e36d9
Enrico Maria Chellini <43685677+bitit-it@users.noreply.github.com>
2020-11-02 21:22:54 +0100

    Update ot_tax.php

--- includes/languages/italiano/modules/order_total/ot_tax.php
+++ includes/languages/italiano/modules/order_total/ot_tax.php
@@ -1,13 +1,13 @@
 <?php
 /*
-  $Id: ot_tax.php,v 1.1 2002/04/03 23:09:49 hpdl Exp $
+  $Id$
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2002 osCommerce 
+  Copyright (c) 2002 osCommerce
 
-  Released under the GNU General Public License 
+  Released under the GNU General Public License
 */
 
   define('MODULE_ORDER_TOTAL_TAX_TITLE', 'Tassa');
