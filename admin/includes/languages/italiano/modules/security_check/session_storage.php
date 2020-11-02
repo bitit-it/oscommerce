c9856bd4fb56325434ca14a65f2f9c973d044bb5
Enrico Maria Chellini <43685677+bitit-it@users.noreply.github.com>
2020-11-02 22:48:31 +0100

    Update session_storage.php

--- admin/includes/languages/italiano/modules/security_check/session_storage.php
+++ admin/includes/languages/italiano/modules/security_check/session_storage.php
@@ -10,6 +10,6 @@
   Released under the GNU General Public License
 */
 
-define('WARNING_SESSION_DIRECTORY_NON_EXISTENT', 'Le cartelle  sessioni non esistono: ' . tep_session_save_path() . '. La sessione non funzioner&agrave; finch&egrave; questa directory non sar&agrave; creata.');
-define('WARNING_SESSION_DIRECTORY_NOT_WRITEABLE', 'Non sono abilitato a scrivere in questa cartella  sessioni : ' . tep_session_save_path() . '. La sessione non funzioner&agrave; finch&egrave; non verranno settati i giusti permessi');
+define('WARNING_SESSION_DIRECTORY_NON_EXISTENT', 'Le cartelle  sessioni non esistono: ' . tep_session_save_path() . '. La sessione non funzionerà; finch&egrave; questa directory non sarà; creata.');
+define('WARNING_SESSION_DIRECTORY_NOT_WRITEABLE', 'Non sono abilitato a scrivere in questa cartella  sessioni : ' . tep_session_save_path() . '. La sessione non funzionerà; finch&egrave; non verranno settati i giusti permessi');
 ?>
